77ccdf56f334071d793b0099bd9c4149288b56ec
gauravrathore701 <gauravrathore701@gmail.com>
2024-08-27 17:24:59 +0530

    Re deploying

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,55 @@
-import 'regenerator-runtime/runtime'
-import "./App.css"
-import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+import "regenerator-runtime/runtime";
+import "./App.css";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
 import useClipboard from "react-use-clipboard";
-import {useState} from "react";
-
+import { useState } from "react";
 
 const App = () => {
-    const [textToCopy, setTextToCopy] = useState();
-    const [isCopied, setCopied] = useClipboard(textToCopy, {
-        successDuration:1000
-    });
+  const [textToCopy, setTextToCopy] = useState();
+  const [isCopied, setCopied] = useClipboard(textToCopy, {
+    successDuration: 1000,
+  });
 
-    //subscribe to thapa technical for more awesome videos
+  const startListening = () =>
+    SpeechRecognition.startListening({ continuous: true, language: "en-IN" });
+  const { transcript, browserSupportsSpeechRecognition } =
+    useSpeechRecognition();
 
-    const startListening = () => SpeechRecognition.startListening({ continuous: true, language: 'en-IN' });
-    const { transcript, browserSupportsSpeechRecognition } = useSpeechRecognition();
+  if (!browserSupportsSpeechRecognition) {
+    return null;
+  }
 
-    if (!browserSupportsSpeechRecognition) {
-        return null
-    }
+  return (
+    <>
+      <div className="container">
+        <h2>Speech to Text Converter</h2>
+        <br />
+        <p>
+          A Speech-to-Text Converter built with React that allows users to
+          transcribe spoken words into written text in real-time. It leverages
+          browser APIs for voice recognition and provides a clean, intuitive
+          interface for seamless transcription. Ideal for accessibility,
+          note-taking, and hands-free tasks.
+        </p>
 
-    return (
-        <>
-            <div className="container">
-                <h2>Speech to Text Converter</h2>
-                <br/>
-                <p>A React hook that converts speech from the microphone to text and makes it available to your React
-                    components.</p>
+        <div className="main-content" onClick={() => setTextToCopy(transcript)}>
+          {transcript}
+        </div>
 
-                <div className="main-content" onClick={() =>  setTextToCopy(transcript)}>
-                    {transcript}
-                </div>
-
-                <div className="btn-style">
-
-                    <button onClick={setCopied}>
-                        {isCopied ? 'Copied!' : 'Copy to clipboard'}
-                    </button>
-                    <button onClick={startListening}>Start Listening</button>
-                    <button onClick={SpeechRecognition.stopListening}>Stop Listening</button>
-
-                </div>
-
-            </div>
-
-        </>
-    );
+        <div className="btn-style">
+          <button onClick={setCopied}>
+            {isCopied ? "Copied!" : "Copy to clipboard"}
+          </button>
+          <button onClick={startListening}>Start Listening</button>
+          <button onClick={SpeechRecognition.stopListening}>
+            Stop Listening
+          </button>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default App;
